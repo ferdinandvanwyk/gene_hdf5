9920151f5d2af5b6c8060537377cbc7cda572c43
ferdinandvwyk <ferdinandvwyk@gmail.com>
2015-02-24 14:16:13 +0000

    update

--- read_hdf5.py
+++ read_hdf5.py
@@ -1,6 +1,9 @@
 import h5py
 import numpy as np
 
+def get_var():
+    return 0
+
 f = h5py.File('mom_Ions.h5', 'r')
 
 tpar = f['/mom_Ions/T_par']
@@ -8,3 +11,4 @@ tpar = f['/mom_Ions/T_par']
 test = tpar['0000000000']
 print('time = ', test.attrs['time'])
 print(test[0,0,0])
+print(f['/mom_Ions/T_par'].items())
