2253a4ec5da4bece7daeab33d5a9ea9128ca8486
Ferdinand van Wyk <ferdinandvwyk@gmail.com>
2015-03-10 15:43:04 +0000

    read relevant fields

--- read_hdf5.py
+++ read_hdf5.py
@@ -1,14 +1,36 @@
 import h5py
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def get_var():
-    return 0
-
+# Read HDF5 file
 f = h5py.File('mom_Ions.h5', 'r')
+mom = f['/mom_Ions']
 
-tpar = f['/mom_Ions/T_par']
+# Read variables
+tpar_h5 = mom['T_par']
+tperp_h5 = mom['T_perp']
+density_h5 = mom['dens']
+qpar_h5 = mom['q_par']
+qperp_h5 = mom['q_perp']
+upar_h5 = mom['u_par']
 
-test = tpar['0000000000']
-print('time = ', test.attrs['time'])
-print(test[0,0,0])
-print(np.array(f['/mom_Ions/time']))
+time = np.array(mom['time'])
+
+# Variable index is in the form of an array of strings indexing time steps
+idx = np.array(mom['T_par'])
+
+density = np.array(density_h5[idx[0]])
+shape = density.shape
+
+# Convert to complex number
+field = np.empty(shape, dtype=complex)
+for i in range(shape[0]):
+    for j in range(shape[1]):
+        for k in range(shape[2]):
+            field[i,j,k] = float(density[i,j,k][0]) + 1j*float(density[i,j,k][1])
+
+plt.contourf(np.real(field[:,:,0]), cmap='coolwarm')
+plt.show()
+
+
